d11e8394ba43424b300f682f01b46673bc886fe6
root <root@localhost.localdomain>
2017-04-06 16:20:22 -0400

    Set gitignore to ignore pyc files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from .__init__ import __version__, __email__, __author__
+
+__version__ = __version__
+__author__  = __author__
+
+requirements = [
+        'facial_recognition'
+        ]
+
+description = "Finds photos in folder with people"
+
+setup(
+        name        = 'photo_finder_py',
+        version     = __version__,
+        author      = __author__,
+
+        )
