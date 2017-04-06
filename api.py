f0a5bb0a7824f6b2848fca8b9a16dc8cb4fa45fd
root <root@localhost.localdomain>
2017-04-06 14:14:25 -0400

    Added files for photo_finder

---
+++ api.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import os
+import face_recognition as fr
+import shutil
+
+def photo_finder():
+    """Finds png, jpeg files in folder in current working directory."""
+    photo_list = []
+    for filename in os.listdir(os.getcwd()):
+        if filename.endswith('.png') or filename.endswith('.jpg'):
+            photo_list.append(filename)
+
+    return photo_list
+
+def face_checker(photo_list):
+    """Finds pictures with faces in list of photos"""
+    face_list = []
+    for photo in photo_list:
+        image = fr.load_image_file(photo)
+        face_locations = fr.face_locations(image)
+        if face_locations:
+            face_list.append(photo)
+
+    return face_list
+
+def check_for_folder():
+    work_dir = os.getcwd()
+    # Check if a 'Faces' folder already exists. This only walks foldera
+    dir_list = os.walk(work_dir).next()[1]
+    count = 0
+    for folder in dir_list:
+        # Logic here doesn't make sense, creates a whole new folder each time it doesn't find Faces
+        if folder != 'Faces':
+            continue
+        else:
+            count += 1
+            return True
+    if count == 0:
+        os.mkdir('Faces')
+
+def move_to_folder(face_list):
+    """Moves photos with Faces to Face Folder"""
+    # TODO: Create logic that allows user to somehow decide where they would like there
+    # faces to be stored.
+    check_for_folder()
+    work_dir = os.getcwd()
+    face_dir = work_dir + '/' + 'Faces'
+    for photo in face_list:
+        path = work_dir + '/' + photo
+        shutil.copy(path, face_dir)
+
+if __name__ == "__main__":
+    photo_list = photo_finder()
+    face_list = face_checker(photo_list)
+    move_to_folder(face_list)
