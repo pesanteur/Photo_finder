ff4ba6e8e79d1261c9d2c90cd9dc52c3fd6eed0f
root <root@localhost.localdomain>
2017-04-10 18:55:22 -0400

    Added a few more modules to __main__. Modules need to be tested

--- __main__.py
+++ __main__.py
@@ -32,6 +32,20 @@ def face_count(photo):
     face_count = len(face_locations)
     return face_count
 
+def find_face(image):
+    # TODO: Create logic for image upload. Where will the main image be placed or sourced from?
+    image = fr.load_image_file(image)
+    image_encoding = fr.face_encodings(image)[0]
+    return image_encoding
+
+def face_match(unknown_image):
+    """Compares main face to faces found in list of photos."""
+    main_face = find_face()
+    unknown_face = fr.face_encodings(unknown_image)[0]
+    results = fr.compare_faces(main_face, unknown_face)
+    # Just return results for now. TODO: be more detailed about who is in the picture
+    return results
+
 if __name__ == "__main__":
     photo_list = photo_finder()
     face_list = face_checker(photo_list)
