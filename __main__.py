a1b3203c969aaf9edb4ef3b6ca4aedfb5c9370c6
root <root@localhost.localdomain>
2017-04-10 18:11:56 -0400

    Created a utils file to handle folder navigation

--- __main__.py
+++ __main__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import os
 import face_recognition as fr
-import shutil
+from utils import check_for_folder
+from utils import move_to_folder
+import os
 
 def photo_finder():
     """Finds png, jpeg files in folder in current working directory."""
@@ -30,32 +31,6 @@ def face_count(photo):
     face_locations = fr.face_locations(image)
     face_count = len(face_locations)
     return face_count
-
-def check_for_folder():
-    # TODO: Create logic that allows user to somehow decide where they would like there
-    # photos with faces to be stored.
-    work_dir = os.getcwd()
-    # Check if a 'Faces' folder already exists. This only walks folders not files.
-    dir_list = os.walk(work_dir).next()[1]
-    count = 0
-    for folder in dir_list:
-        # Logic here doesn't make sense, creates a whole new folder each time it doesn't find Faces
-        if folder != 'Faces':
-            continue
-        else:
-            count += 1
-            return True
-    if count == 0:
-        os.mkdir('Faces')
-
-def move_to_folder(face_list):
-    """Moves photos with Faces to Face Folder"""
-    check_for_folder()
-    work_dir = os.getcwd()
-    face_dir = work_dir + '/' + 'Faces'
-    for photo in face_list:
-        path = work_dir + '/' + photo
-        shutil.copy(path, face_dir)
 
 if __name__ == "__main__":
     photo_list = photo_finder()
