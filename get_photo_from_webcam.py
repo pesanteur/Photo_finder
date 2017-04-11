1c40927da279c940632c371d17c41af548d2b2c3
root <root@localhost.localdomain>
2017-04-11 13:37:13 -0400

    Tested sample webcam module on MacOs

--- get_photo_from_webcam.py
+++ get_photo_from_webcam.py
@@ -4,6 +4,7 @@ from cv2 import *
 # Sample code for taking photo with webcam. This will be used to help find all photos
 # on a computer with your face in it and organize those accordingly
 # Get a reference to camera
+# Code works on MacOS but not yet on Linux
 cam = VideoCapture(0)
 s, img = cam.read()
 if s:
