ed2884c346dc7096ab6120e7875e59190cd9eb3c
root <root@localhost.localdomain>
2017-04-10 21:12:45 -0400

    Changed api.py to __main__.py

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ __author__      = """Mathue Duhaney"""
 __email__       = "pesanteur123@gmail.com"
 __version__     = "0.1.0"
 
-from .api import photo_finder, face_checker, check_for_folder, move_to_folder, face_count
+from .main import photo_finder, face_checker, check_for_folder, move_to_folder, face_count
