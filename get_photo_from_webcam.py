3cd1721fffe5721eae07b789c3824eb624385779
root <root@localhost.localdomain>
2017-04-10 23:10:57 -0400

    added test webcam file, for future webcam access

---
+++ get_photo_from_webcam.py
@@ -0,0 +1,14 @@
+import face_recognition as fr
+from cv2 import *
+
+# Sample code for taking photo with webcam. This will be used to help find all photos
+# on a computer with your face in it and organize those accordingly
+# Get a reference to camera
+cam = VideoCapture(0)
+s, img = cam.read()
+if s:
+    namedWIndow("cam-test", CV_WINDOW_AUTOSIZE)
+    imshow('cam-test', img)
+    waitKey(0)
+    destroyWindow('cam-test')
+    imwrite('photoofme.jpg', img) #Save image
